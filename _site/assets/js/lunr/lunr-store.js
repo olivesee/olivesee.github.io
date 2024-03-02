58aba730fac40a46fd206593969e6e5d2ca5f542
SeoKJun Yoon <infoseokjun94@gmail.com>
2024-03-02 19:10:02 +0900

    [24.03.02] update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "chapter 1 첫 게시물 예시",
+        "excerpt":"위 형식을 참고하여 블로그를 커스텀하시오   - 게시물 업로드 양식   위 형식을 참고하여 블로그를 커스텀하시오   게시물 업로드 양식  ","categories": ["Blog"],
+        "tags": ["Blog","Git","Github"],
+        "url": "http://localhost:4000/blog/git-&-github/",
+        "teaser": null
+      }]
