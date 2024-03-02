be6fa6e7d523530d0f0ff162f50ecb160e111ca3
SeoKJun Yoon <infoseokjun94@gmail.com>
2024-03-02 19:11:53 +0900

    [24.03.02] update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "chapter 1 첫 게시물 예시",
         "excerpt":"위 형식을 참고하여 블로그를 커스텀하시오   - 게시물 업로드 양식   위 형식을 참고하여 블로그를 커스텀하시오   게시물 업로드 양식  ","categories": ["Blog"],
         "tags": ["Blog","Git","Github"],
-        "url": "http://localhost:4000/blog/git-&-github/",
+        "url": "https://olivesee.github.io/blog/git-&-github/",
         "teaser": null
       }]
